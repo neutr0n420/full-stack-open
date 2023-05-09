37054ca6b85c13fb494e7ee8ef7f92c217dab10f
aryan bramhane <aryanbramhane1@gmail.com>
2023-05-09 13:46:18 +0530

    completed exersice 2.2 :

--- part-2/courseinfo/src/App.jsx
+++ part-2/courseinfo/src/App.jsx
@@ -1,28 +1,52 @@
+import { nanoid } from "nanoid"
 import Course from "./component/Course"
 
 
 function App() {
-  const course = {
+  const course = 
+  [
+  {
     name: 'Half Stack application development',
     parts: [
       {
+        id: nanoid(),
         name: 'Fundamentals of React',
         exercises: 10
       },
       {
+        id: nanoid(),
         name: 'Using props to pass data',
         exercises: 7
       },
       {
+        id: nanoid(),
         name: 'State of a component',
         exercises: 14
       },
       {
+        id: nanoid(),
         name: 'Redux',
         exercises: 11
       }
     ]
+  },
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+    ]
   }
+]
 
   
 
--- part-2/courseinfo/src/component/Course.jsx
+++ part-2/courseinfo/src/component/Course.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-
-
-const Header = ({course}) => <h2>{course}</h2>
+const Header = ({course}) => <h2>{course[0].name}</h2>
 const Part = (props) => <p>{props.name} {props.exercise}</p>
 
 
 // so Here we are making a new component named content which will render the whole part section.lets look it line by line
 //initlaizing the function content and taking the parameter by using the destrucuting the props method
 const Content = ({course}) =>{
+    // console.log(course[0])
     // In the variable name we are storing the the part name and the exersices that are coming. Here we are itrating over every single part by using the map function and combining the part name with the part exersice in the one part only. 
-    const name = course.parts.map((part,i)=> <p key={i}>{part.name} {part.exercises}</p>)
+    const name = course[0].parts.map((part)=> <p key={part.id}>{part.name} {part.exercises}</p>)
     //This above line is really important.
+    // Here we are rendering the name component inside a div.
     return(
         <div>
             {name} 
@@ -18,12 +18,26 @@ const Content = ({course}) =>{
     )
 }
 
+const Sum = ({course}) =>{
+    const newSum = course[0].parts.reduce((x,y)=>{
+        return x + y.exercises
+    },0)
+    return(
+        // <p>The Total is {course.parts[0].exercises +course.parts[1].exercises+course.parts[2].exercises+course.parts[3].exercises} </p>
+        <p>total of {newSum} exercise</p>
+    )
+    
+    
+    // const total = course.exercises.reduce()
+}
+
 
 const Course = ({course}) =>{
     return(
         <>
-           <Header course = {course.name}/>
+           <Header course = {course}/>
            <Content course = {course}/>
+           <Sum course={course}/>
         </>
     )
 }
