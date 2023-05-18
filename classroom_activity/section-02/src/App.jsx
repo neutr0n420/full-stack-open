18874c1cee065f8c14d61d8cea56f69b8dea1a0b
aryan bramhane <aryanbramhane1@gmail.com>
2023-05-18 11:37:29 +0530

    using Rest api to modify data

--- classroom_activity/section-02/src/App.jsx
+++ classroom_activity/section-02/src/App.jsx
@@ -1,46 +1,58 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
-import axios from 'axios'
+import noteService from './services/notes'
 
 const App = () =>{
-  // Here we added a new notes and initalized with the notes array which is been defined at Main.jsx page.
+  
     const [notes, setNotes] = useState([])
-   // Here we are creating a new note and initized with the empty string.
     const [newNote , setNewNote] = useState("")
 
 
     useEffect(() =>{
-      console.log("effect")
-      axios
-        .get('http://localhost:3001/notes')
-        .then(response =>{
-          console.log("response fulfiled")
-          setNotes(response.data)
+      noteService
+        .getAll()
+        .then(initlaValue =>{
+          setNotes(initlaValue)
         })
     }, []) 
 
     
-    console.log('render', notes.length, 'notes')
     //We will call  this function when the form is getting submited.
     const addNote = (event) =>{
-      // Here we are preventing the default things to happen such as refesh after the forms get submitted.
       event.preventDefault()
-      //The target of the event stored in event.target is logged to the console.
-      // we want to access the element in it for now.
-      console.log("button clicked",event.target)
       const noteObject = {
         content: newNote,
         important: Math.random() < 0.5,
         id: notes.length+1
       }
-      setNotes(notes.concat(noteObject))
+
+        noteService
+          .create(noteObject)
+          .then(retunedNote =>{
+            setNotes(notes.concat(retunedNote))
+            setNewNote('')
+          })
     }
+    
     const handleNewNote = (event) =>{
       console.log(event.target.value)
       setNewNote(event.target.value)
     }
     
+    const toggleImportanceOf = (id) =>{
+      // find keyword returns the first element that satisfy the provided keyword.
+      const note = notes.find(n => n.id === id)
+      // Here we will be creating a new Note that we want to modify and here we are copying the exact same thing and then toggling the important to not important
+      const changedNote = {...note, important: !note.important}
 
+      noteService
+        .update(changedNote, id)
+        .then(retunedNote =>{
+          setNotes(notes.map(note => note.id !== id? note : retunedNote))
+        })
+
+    }
+    console.log(noteService.getAll)
     return(
         <div>
         <h1>Notes</h1>
@@ -48,9 +60,12 @@ const App = () =>{
         </div>
         <ul>
         {notes.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+            key={note.id} 
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+            />
         )}
-        {/* On submit the form we are adding the new note here. This is calling the new funciton called as 'Addnote'. */}
         <form onSubmit={addNote}>
           <input type="text" value={newNote} onChange={handleNewNote} />
           <button >Submit</button>
