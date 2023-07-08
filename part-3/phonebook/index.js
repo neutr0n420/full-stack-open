4e9af3462c5b2c63d39dbb75cca259fb355af0e9
aryan bramhane <aryanbramhane1@gmail.com>
2023-07-08 21:44:12 +0530

    feat: exersice 3.2 completed

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -24,6 +24,7 @@ const contacts = [
     }
 ]
 
+
 app.get('/', (request, response)=>{
     response.send("<h1>Hello Welcome to the server</h1>")
 })
@@ -31,9 +32,21 @@ app.get('/', (request, response)=>{
 app.get('/api/persons', (request, response)=>{
     response.send(contacts)
 })
-
-
-
+const dateTime = ()=>{
+  const date = new Date()
+  const weekDays = ["Sun", "Mon", "Tue", "Wen", "Thr", "Fri", "Sat"]
+  const Months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+  const month = Months[date.getMonth()]
+  const day = weekDays[date.getDay()]
+  return(`${day} ${month}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+}
+// const currentDate = date.toLocaleString()
+app.get('/info', (request, response)=>{
+  const noOfEntries = contacts.length
+  response.send(`Phonebook has info for ${noOfEntries} peoples <br/>
+    ${dateTime()}
+  `)
+})
 
 const PORT = 3002
 app.listen(PORT, ()=>{
