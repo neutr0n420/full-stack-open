f8ac189225492e5913c12ae5aae7824a1affc952
aryan bramhane <aryanbramhane1@gmail.com>
2023-08-08 23:27:06 +0530

    feat: donw with the core functionality

---
+++ part-3/phonebook/server/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const password = process.argv[2]
+const uri = `mongodb+srv://root:${password}@cluster0.onmb8nt.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000, // Adjust the timeout value as needed
+}).then(() => {
+  console.log('Connected to MongoDB successfully!');
+  const noteSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  });
+
+
+  const Phonebook = mongoose.model('phonebook', noteSchema);
+
+  const phoneNumber = new Phonebook({
+    name: personName,
+    number: personNumber,
+  });
+ 
+  phoneNumber.save().then((result) => {
+    console.log("The contact had been saved")
+    // mongoose.connection.close();
+    
+  });
+if(process.argv === 3){
+
+}
+  Phonebook.find({}).then(result =>{
+    result.forEach((phonebook)=>{
+      console.log(phonebook.name)
+      console.log(phonebook.number)
+    })  
+
+  })
+}).catch((error) => {
+  console.log('Failed to connect to MongoDB:', error.message);
+});
